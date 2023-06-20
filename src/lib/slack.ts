408d2a81f23f76467778a5c3541da438354b03a9
kaz <kazuki.amatatsu.fd@gmail.com>
2023-06-20 12:03:07 +0900

    test

--- src/lib/slack.ts
+++ src/lib/slack.ts
@@ -7,18 +7,21 @@ export const notificationErrorToSlack = async (error: Error) => {
   if (!SLACK_WEBHOOK_URL) return;
 
   // KVからSlackの通知の最終更新日時を取得する
-  const lastNotificationDate = await kv.get<Date>("lastNotificationDate");
+  const lastNotification = await kv.get<Date>("lastNotificationDate");
+  const lastNotificationDate = lastNotification ? new Date(lastNotification) : null;
 
   // 24時間前の日時を取得する
   const now = new Date();
-  const yesterday = new Date(now.setDate(now.getDate() - 1));
+  // const previous = new Date(now.setDate(now.getDate() - 1));
+  const previous = new Date(now.setMinutes(now.getMinutes() - 1)); // 1分前
 
   // 24時間前の日時がKVに保存されている日時よりも新しい場合はSlackに通知する
-  if (lastNotificationDate && lastNotificationDate < yesterday) {
+  if (lastNotificationDate ?? now < previous) {
     await axios.post(
       SLACK_WEBHOOK_URL,
       {
         message: error.message,
+        time: (lastNotificationDate ?? now).toDateString(),
       },
       { headers: { "Content-Type": "application/json" } }
     );
